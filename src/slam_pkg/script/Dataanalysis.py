a6ee4ab82590e8f0675de230ca13a8fbe95083e4
CoCoKayya18 <collins1999.ck@gmail.com>
2024-03-14 00:09:52 +0100

    Data standardization implemented, still need to be further developed

--- src/slam_pkg/script/Dataanalysis.py
+++ src/slam_pkg/script/Dataanalysis.py
@@ -12,3 +12,15 @@ print(data_df.describe())
 
 # Check for any missing values
 print(data_df.isnull().sum())
+
+data_path = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/data/StandardizedData.csv'
+data_df = pd.read_csv(data_path)
+
+# Display basic information about the dataset
+print(data_df.info())
+
+# Get statistical summaries of the data
+print(data_df.describe())
+
+# Check for any missing values
+print(data_df.isnull().sum())
