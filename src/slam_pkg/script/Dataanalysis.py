e25d5dce0f4f5eb139c81ae4a349ecba6cf0a3d9
CoCoKayya18 <collins1999.ck@gmail.com>
2024-03-13 17:34:41 +0100

    k-mean++ clustering implemented

---
+++ src/slam_pkg/script/Dataanalysis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Load the data
+data_path = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/data/Data.csv'
+data_df = pd.read_csv(data_path)
+
+# Display basic information about the dataset
+print(data_df.info())
+
+# Get statistical summaries of the data
+print(data_df.describe())
+
+# Check for any missing values
+print(data_df.isnull().sum())
--- src/slam_pkg/script/ModelTesting.py
+++ src/slam_pkg/script/ModelTesting.py
@@ -1,9 +1,15 @@
-import GPy
-from zipfile import ZipFile
+from sklearn.gaussian_process import GaussianProcessRegressor
+import joblib
 
 
-with ZipFile("/home/cocokayya18/Spezialisierung-1/src/slam_pkg/myMLmodel/GP_Model.json.zip", 'r') as zObject: 
-    zObject.extractall(path="/home/cocokayya18/Spezialisierung-1/src/slam_pkg/myMLmodel")
+gp_loaded = joblib.load('/home/cocokayya18/Spezialisierung-1/src/slam_pkg/myMLmodel/GP_model.joblib')
 
+x_predict = [[-2.000019313172563],[-0.4999996481424659],[-0.00021734587417554066],[8.57819098239314e-08],[6.18047277926345e-07],[9.128718510181314e-06]]
 
-# myModel = GPy.load('/home/cocokayya18/Spezialisierung-1/src/slam_pkg/myMLmodel/GP_Model.json')
+y_predict = gp_loaded.predict(x_predict, return_std=True)
+
+print(y_predict)
+
+print(gp_loaded.get_params())
+
+print("GT_X: -2.2288948464677105e-09, GT_Y: 1.6343130682106022e-08, GT_Yaw: 1.4877646695738464e-05")
