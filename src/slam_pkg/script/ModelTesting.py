717680daeb4e7ab4f4e534be81740c71812b5c78
CoCoKayya18 <collins1999.ck@gmail.com>
2024-03-17 17:35:30 +0100

    3D plot added, hyperparametertuning in progress

--- src/slam_pkg/script/ModelTesting.py
+++ src/slam_pkg/script/ModelTesting.py
@@ -31,25 +31,27 @@ datafilepath = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/data'
 modelFilePath = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/myMLmodel'
 scalerFilePath = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/Scaler'
 
-ith_datapoint = 10000
+ith_datapoint = 1
+isSparse = 'sparse0_'
+# isSparse = ''
 
-model_filename = f'gpy_model_{ith_datapoint}DP.pkl'
-scaler_filenameX = f'scaler_X_{ith_datapoint}.pkl'
-scaler_filenameY = f'scaler_Y_{ith_datapoint}.pkl'
+model_filename = f'{isSparse}gpy_model_{ith_datapoint}DP.pkl'
+scaler_filenameX = f'{isSparse}scaler_X_{ith_datapoint}.pkl'
+scaler_filenameY = f'{isSparse}scaler_Y_{ith_datapoint}.pkl'
 
 # Load the model
 with open(os.path.join(modelFilePath, model_filename), 'rb') as file:
     loaded_model = pickle.load(file)
 
 # Get the data out of the csv
-dataframe = pandas.read_csv(os.path.join(datafilepath, 'Data.csv'))
+dataframe = pandas.read_csv(os.path.join(datafilepath, f'{isSparse}{ith_datapoint}_DP_train_data.csv'))
 features = ['Ground_Truth_X', 'Ground_Truth_Y', 'Ground_Truth_Yaw', 'Velocity_Linear_X', 'Velocity_Linear_Y', 'Velocity_Angular_Yaw']
 target = ['Delta_X_X', 'Delta_X_Y', 'delta_X_Yaw']
 
 #get every i-th datapoint out of the csv
-ithDataframe = dataframe[::ith_datapoint]
-X = ithDataframe[features].values
-Y = ithDataframe[target].values
+# ithDataframe = dataframe[::ith_datapoint]
+X_train = dataframe[features].values
+Y_train = dataframe[target].values
 
 with open(os.path.join(scalerFilePath, scaler_filenameX), 'rb') as file:
     scaler_X = pickle.load(file)
@@ -57,12 +59,12 @@ with open(os.path.join(scalerFilePath, scaler_filenameX), 'rb') as file:
 with open(os.path.join(scalerFilePath, scaler_filenameY), 'rb') as file:
     scaler_Y = pickle.load(file)
 
-X_standardized = scaler_X.transform(X)
-Y_standardized = scaler_Y.transform(Y)
+# X_standardized = scaler_X.transform(X)
+# Y_standardized = scaler_Y.transform(Y)
 
-# Split the data into training, validation and testing sets and save
-X_train, X_temp, Y_train, Y_temp = train_test_split(X_standardized, Y_standardized, test_size=0.3, random_state=42)
-X_val, X_test, Y_val, Y_test = train_test_split(X_temp, Y_temp, test_size=0.5, random_state=42)
+# # Split the data into training, validation and testing sets and save
+# X_train, X_temp, Y_train, Y_temp = train_test_split(X_standardized, Y_standardized, test_size=0.3, random_state=42)
+# X_val, X_test, Y_val, Y_test = train_test_split(X_temp, Y_temp, test_size=0.5, random_state=42)
 
 # Predict using the loaded model
 y_predict_mean, y_predict_variance = loaded_model.predict(X_train)
@@ -78,7 +80,7 @@ predicted_means_rescaled = scaler_Y.inverse_transform(y_predict_mean)
 real_values = scaler_Y.inverse_transform(Y_train)
 predicted_variances = y_predict_variance
 
-filename = f'{ith_datapoint}_DP_predictions_vs_real.csv'
+filename = f'{isSparse}{ith_datapoint}_DP_predictions_vs_real.csv'
 
 file_path = os.path.join(datafilepath, filename)
 
--- src/slam_pkg/script/SparseModelTraining.py
+++ src/slam_pkg/script/SparseModelTraining.py
@@ -4,12 +4,16 @@ import os
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import pandas
 
 datafilepath = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/data'
 modelFilePath = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/myMLmodel'
 scalerFilePath = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/Scaler'
 
 ith_datapoint = 1
+sparse_number = 0
+isSparse = 'sparse'
+# isSparse = ''
 
 # Load data
 dataframe = pd.read_csv(os.path.join(datafilepath, 'Data.csv'))
@@ -30,6 +34,18 @@ Y_standardized = scaler_Y.fit_transform(Y)
 # Split the data into training, validation, and testing sets
 X_train, X_temp, Y_train, Y_temp = train_test_split(X_standardized, Y_standardized, test_size=0.3, random_state=42)
 X_val, X_test, Y_val, Y_test = train_test_split(X_temp, Y_temp, test_size=0.5, random_state=42)
+df_X_Train = pandas.DataFrame(X_train, columns=features)
+df_X_Val = pandas.DataFrame(X_val, columns=features)
+df_X_Test = pandas.DataFrame(X_test, columns=features)
+df_Y_Train = pandas.DataFrame(Y_train, columns=target)
+df_Y_Val = pandas.DataFrame(Y_val, columns=target)
+df_Y_Test = pandas.DataFrame(Y_test, columns=target)
+train_data = pandas.concat([df_X_Train, df_Y_Train], axis=1)
+val_data = pandas.concat([df_X_Val, df_Y_Val], axis=1)
+test_data = pandas.concat([df_X_Test, df_Y_Test], axis=1)
+train_data.to_csv(os.path.join(datafilepath, f'{isSparse}{ith_datapoint}_DP_train_data.csv'), index=False)
+val_data.to_csv(os.path.join(datafilepath, f'{isSparse}{ith_datapoint}_DP_val_data.csv'), index=False)
+test_data.to_csv(os.path.join(datafilepath, f'{isSparse}{ith_datapoint}_DP_test_data.csv'), index=False)
 
 # Define the kernel with RBF for the function and WhiteKernel for noise level
 kernel = GPy.kern.RBF(input_dim=X_train.shape[1], variance=1.0, lengthscale=1.0) + GPy.kern.White(X_train.shape[1], variance=1.0)
@@ -44,7 +60,7 @@ sparse_model.optimize_restarts(num_restarts=10, verbose=True)
 print(sparse_model)
 
 # Save the model and scalers
-model_filename = f'sparse_gpy_model_{ith_datapoint}DP.pkl'
+model_filename = f'sparse{sparse_number}_gpy_model_{ith_datapoint}DP.pkl'
 scaler_filenameX = f'sparse_scaler_X_{ith_datapoint}.pkl'
 scaler_filenameY = f'sparse_scaler_Y_{ith_datapoint}.pkl'
 
