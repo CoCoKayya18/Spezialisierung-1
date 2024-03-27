92be13913057d4b144d0e64050125eea4c3dfd90
CoCoKayya18 <collins1999.ck@gmail.com>
2024-03-28 00:42:52 +0100

    Kinematic Model calculations are on the way

--- src/slam_pkg/script/RosbagToCsv.py
+++ src/slam_pkg/script/RosbagToCsv.py
@@ -49,6 +49,52 @@ class BagDataProcessor:
         df['angular_acceleration_yaw'] = df['angular_velocity_yaw'].diff() / time_diffs
         
         return df[['Time', 'linear_velocity_x', 'angular_velocity_yaw', 'linear_acceleration_x', 'angular_acceleration_yaw']]
+    
+    def calculate_kinematic_deltas(self, df):
+        df['kinematic_delta_x'] = 0.0
+        df['kinematic_delta_y'] = 0.0
+        df['kinematic_delta_yaw'] = 0.0
+        
+        # Initial pose
+        x, y, theta = 0.0, 1.0, 0.0
+        
+        for index, row in df.iterrows():
+            if index == 0:
+                continue
+            vc = row['linear_velocity_x']
+            wc = row['angular_velocity_yaw']
+            dt = 0.0333
+            
+            # Skip the first row or any rows where dt <= 0
+            if dt <= 0:
+                continue
+                
+            delta_x, delta_y, delta_theta = self.calculate_robot_pose_change(vc, wc, theta, dt)
+            
+            df.at[index, 'kinematic_delta_x'] = delta_x
+            df.at[index, 'kinematic_delta_y'] = delta_y
+            df.at[index, 'kinematic_delta_yaw'] = delta_theta
+            
+            # Update the pose
+            x += delta_x
+            y += delta_y
+            theta += delta_theta
+        
+        return df
+
+    def calculate_robot_pose_change(self, vc, wc, theta, dt):
+        # Kinematic model for differential drive robot
+        if wc == 0:
+            # Straight line movement
+            calculated_delta_x = vc * dt
+            calculated_delta_y = 0.0
+        else:
+            # Arc movement
+            calculated_delta_x = (vc / wc) * (np.sin(theta + wc * dt) - np.sin(theta))
+            calculated_delta_y = (vc / wc) * (-np.cos(theta + wc * dt) + np.cos(theta))
+        calculated_delta_theta = wc * dt
+
+        return calculated_delta_x, calculated_delta_y, calculated_delta_theta
 
     def process_and_save_data(self, ground_truth_df, joint_state_df):
         processed_gt_df = self.calculate_ground_truth_deltas(ground_truth_df)
@@ -56,13 +102,16 @@ class BagDataProcessor:
 
         dataFilePathDeltas = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/data/GT_Deltas.csv'
         dataFilePathVelsAndAccs = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/data/Vels_And_Accels.csv'
-        mergedPath = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/data/Data.csv'
+        mergedPath = '/home/cocokayya18/Spezialisierung-1/src/slam_pkg/data/Data_Testing.csv'
 
         # Remove rows with any NaN values (which now includes the original 'inf' values)
         processed_gt_df.replace([np.inf, -np.inf], np.nan, inplace=True)
         processed_gt_df = processed_gt_df.dropna()
         processed_joint_df.replace([np.inf, -np.inf], np.nan, inplace=True)
         processed_joint_df = processed_joint_df.dropna()
+
+        # Calculate the kinematic Delta_X too and add to DF
+        processed_joint_df = self.calculate_kinematic_deltas(processed_joint_df)
 
         processed_gt_df.to_csv(dataFilePathDeltas, index=False)
         processed_joint_df.to_csv(dataFilePathVelsAndAccs, index=False)
